19ddec29ac8c59253dfe9325270d981f6e1bc3c2
pinbal-wizard <68714330+pinbal-wizard@users.noreply.github.com>
2023-04-05 12:05:26 +1000

    CBRC_CTF

--- Networking/Flags/00 Splitting strings.py
+++ Networking/Flags/00 Splitting strings.py
@@ -1,3 +1,27 @@
+
+
+
+def main2():
+    phrases=[]
+    phrasesSort = []
+    with open ("input.txt") as f:
+        for line in f:
+            line = line.strip()
+            phrase = line.split(" ")
+            #print(phrase)
+            phrase[0] = phrase[0]
+            phrase[1] = phrase[1]
+            phrases.append(phrase)
+    for item in phrases:
+        phrasesSort.append(f"({item[1]},{item[0]})")
+    output = "cbrc_CTF{"
+    for i in range(len(phrasesSort)-1):
+        output += f"{phrasesSort[i]}"
+    output+=phrasesSort[-1]
+    output+="}"
+    print(output)
+
+
 def main():
     with open("input.txt") as f:
         lines = f.read().splitlines()
@@ -7,10 +31,10 @@ def main():
             line =','.join([b.upper(),a.upper()])
             line = f"({line})"
             newLines.append(line)
-        return("cbrc_CTF{" + ':'.join(newLines) + "}")
+        return("cbrc_CTF{" + ''.join(newLines) + "}")
 
 print(main())
-
+print()
 
 """
 All Upper : : 

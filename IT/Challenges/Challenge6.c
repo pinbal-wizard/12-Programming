094a500685eed4ebe9f568a0204271c968a1bc76
pinbal-wizard <68714330+pinbal-wizard@users.noreply.github.com>
2023-04-06 23:42:36 +1000

    changed intellisence settings

--- IT/Challenges/Challenge6.c
+++ IT/Challenges/Challenge6.c
@@ -2,15 +2,14 @@
 #include "LinkedList.h"
 
 
-//void listPartition(LinkedList list, int x){
-//
-//}
+void listPartition(LinkedList list, int x){
+
+}
 
 int main(void) {
-    printf("fuck you");
-    // LinkedList list = {(void*)10, NULL};
-    // for(int i = 0; i < 10; ++i){
-    //     append(&list, (void*)i);
-    // }
-    // printValues(&list, LList);
+    LinkedList list = {(void*)10, NULL};
+    for(int i = 0; i < 10; ++i){
+        append(&list, (void*)i);
+    }
+    printValues(&list, LList);
 }
--- Macros/ccg.bat
+++ Macros/ccg.bat
@@ -1,2 +1,3 @@
+@echo off
 gcc %1 -o %1.exe -I "%cd%\IT\Include"
 %cd%/%1.exe
